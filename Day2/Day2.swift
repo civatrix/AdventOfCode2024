19c8957ccf7b2b172684ad64b09266f7574d9a93
Daniel Johns <civatrix@gmail.com>
2024-12-01 21:09:29 -0800

    Day 2 Part 1

--- Day2/Day2.swift
+++ Day2/Day2.swift
@@ -9,6 +9,23 @@ import Foundation
 
 final class Day2: Day {
     func run(input: String) -> String {
-        return ""
+        return input.lines
+            .map { $0.allDigits }
+            .map {
+                let increasing = $0.first! < $0.last!
+                for pair in $0.adjacentPairs() {
+                    if increasing && pair.0 > pair.1 {
+                        return 0
+                    } else if !increasing && pair.0 < pair.1 {
+                        return 0
+                    }
+                    let difference = abs(pair.0 - pair.1)
+                    if !(1...3).contains(difference) {
+                        return 0
+                    }
+                }
+                return 1
+            }
+            .sum.description
     }
 }
--- Day2/Day2Tests.swift
+++ Day2/Day2Tests.swift
@@ -13,7 +13,13 @@ final class Day2Tests: XCTestCase {
     func testDay() throws {
         let input =
 """
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
 """
-        XCTAssertEqual(day.run(input: input), "")
+        XCTAssertEqual(day.run(input: input), "2")
     }
 }
