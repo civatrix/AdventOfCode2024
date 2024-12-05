f9d3d34f4be15d47c90d4d96a430409ef0bd4516
Daniel Johns <civatrix@gmail.com>
2024-12-05 08:40:29 -0800

    Day 5 Part 1

--- Day5/Day5.swift
+++ Day5/Day5.swift
@@ -9,6 +9,35 @@ import Foundation
 
 final class Day5: Day {
     func run(input: String) -> String {
-        return ""
+        var before = [Int: Set<Int>]()
+        var after = [Int: Set<Int>]()
+        var lines = input.lines.map { $0.allDigits }.makeIterator()
+        var nextLine = lines.next()
+        while let line = nextLine {
+            guard line.count == 2 else {
+                break
+            }
+            
+            before[line[1], default: []].insert(line[0])
+            after[line[0], default: []].insert(line[1])
+            nextLine = lines.next()
+        }
+        
+        var total = 0
+        while let line = nextLine {
+            let isCorrect = line.indices.allSatisfy { index in
+                let prefix = line.prefix(upTo: index)
+                let suffix = line.suffix(from: index + 1)
+                
+                return before[line[index], default: []].isSuperset(of: prefix) && after[line[index], default: []].isSuperset(of: suffix)
+            }
+            
+            if isCorrect {
+                total += line[line.count / 2]
+            }
+            nextLine = lines.next()
+        }
+        
+        return total.description
     }
 }
