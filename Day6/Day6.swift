0ad28f1a0b0797a9678995fdbb83c8356b02af96
Daniel Johns <civatrix@gmail.com>
2024-12-05 22:29:46 -0800

    Day 6 Part 1

--- Day6/Day6.swift
+++ Day6/Day6.swift
@@ -9,6 +9,34 @@ import Foundation
 
 final class Day6: Day {
     func run(input: String) -> String {
-        return ""
+        var grid = Set<Point>()
+        var position = Point.zero
+        var direction = Point.up
+        
+        let lines = input.lines
+        for (y, line) in lines.enumerated() {
+            for (x, cell) in line.enumerated() {
+                if cell == "#" {
+                    grid.insert([x, y])
+                } else if cell == "^" {
+                    position = [x, y]
+                }
+            }
+        }
+        
+        let xBoundary = 0 ..< lines[0].count
+        let yBoundary = 0 ..< lines.count
+        
+        var visited = Set<Point>()
+        while xBoundary.contains(position.x) && yBoundary.contains(position.y) {
+            visited.insert(position)
+            if grid.contains(position + direction) {
+                direction = direction.rotate(clockwise: true)
+            } else {
+                position += direction
+            }
+        }
+        
+        return visited.count.description
     }
 }
