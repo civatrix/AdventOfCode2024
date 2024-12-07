9feb6e02a0a113ef56faef2cbcd0e57c08f4d05d
Daniel Johns <civatrix@gmail.com>
2024-12-06 21:09:11 -0800

    Day 7 Part 1

--- Day7/Day7.swift
+++ Day7/Day7.swift
@@ -9,6 +9,27 @@ import Foundation
 
 final class Day7: Day {
     func run(input: String) -> String {
-        return ""
+        return input.lines.map { line in
+            var digits = line.allDigits
+            let total = digits.removeFirst()
+            let matches = recur(numbers: digits, nextOperator: +, target: total) || recur(numbers: digits, nextOperator: *, target: total)
+            
+            return matches ? total : 0
+        }
+        .sum.description
+    }
+    
+    func recur(numbers: [Int], nextOperator: (Int, Int) -> Int, target: Int) -> Bool {
+        guard numbers.count > 1 else {
+            return numbers[0] == target
+        }
+        
+        let total = nextOperator(numbers[0], numbers[1])
+        if total > target {
+            return false
+        }
+        
+        let nextNumbers = [total] + numbers.dropFirst(2)
+        return recur(numbers: nextNumbers, nextOperator: +, target: target) || recur(numbers: nextNumbers, nextOperator: *, target: target)
     }
 }
--- Day7/Day7Tests.swift
+++ Day7/Day7Tests.swift
@@ -13,7 +13,16 @@ final class Day7Tests: XCTestCase {
     func testDay() throws {
         let input =
 """
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
 """
-        XCTAssertEqual(day.run(input: input), "")
+        XCTAssertEqual(day.run(input: input), "3749")
     }
 }
