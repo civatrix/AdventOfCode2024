30945ff3137ba356de2a2da006eaf38fcd106d06
Daniel Johns <civatrix@gmail.com>
2024-12-10 12:14:25 -0800

    Day 10 Part 1

--- Day10/Day10.swift
+++ Day10/Day10.swift
@@ -8,7 +8,30 @@
 import Foundation
 
 final class Day10: Day {
+    var map = [Point: Int]()
     func run(input: String) -> String {
-        return ""
+        var trailheads = [Point]()
+        for (y, line) in input.lines.enumerated() {
+            for (x, cell) in line.enumerated() {
+                map[[x, y]] = cell.wholeNumberValue ?? -1
+                if cell == "0" {
+                    trailheads.append([x, y])
+                }
+            }
+        }
+        
+        return trailheads.map { destinations(from: $0).count }.sum.description
+    }
+    
+    func destinations(from: Point) -> Set<Point> {
+        if map[from] == 9 {
+            return [from]
+        }
+        
+        let nextElevation = map[from]! + 1
+        return from.adjacent
+            .filter { map[$0, default: -1] == nextElevation }
+            .map { destinations(from: $0) }
+            .reduce(into: [], { $0.formUnion($1) })
     }
 }
--- Day10/Day10Tests.swift
+++ Day10/Day10Tests.swift
@@ -13,7 +13,43 @@ final class Day10Tests: XCTestCase {
     func testDay() throws {
         let input =
 """
+89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732
 """
-        XCTAssertEqual(day.run(input: input), "")
+        XCTAssertEqual(day.run(input: input), "36")
+    }
+    
+    func testSimple() throws {
+        let input =
+"""
+...0...
+...1...
+...2...
+6543456
+7.....7
+8.....8
+9.....9
+"""
+        XCTAssertEqual(day.run(input: input), "2")
+    }
+    
+    func test2() throws {
+        let input =
+"""
+..90..9
+...1.98
+...2..7
+6543456
+765.987
+876....
+987....
+"""
+        XCTAssertEqual(day.run(input: input), "4")
     }
 }
