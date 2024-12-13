36367515f6331e32c404a124827950afa2877977
Daniel Johns <civatrix@gmail.com>
2024-12-12 21:17:56 -0800

    Day 13 Part 1

--- Day13/Day13.swift
+++ Day13/Day13.swift
@@ -9,6 +9,28 @@ import Foundation
 
 final class Day13: Day {
     func run(input: String) -> String {
-        return ""
+        input.allDigits.chunks(ofCount: 6).map { game in
+            let a = Point(x: game[n: 0], y: game[n: 1])
+            let b = Point(x: game[n: 2], y: game[n: 3])
+            let prize = Point(x: game[n: 4], y: game[n: 5])
+            
+            var minTotal: Int?
+            for aCount in 0 ... 100 {
+                let aPosition = a * aCount
+                guard aPosition.x <= prize.x && aPosition.y <= prize.y else { break }
+                for bCount in 0 ... 100 {
+                    let bPosition = b * bCount + aPosition
+                    if bPosition == prize {
+                        minTotal = min(minTotal ?? .max, aCount * 3 + bCount)
+                    } else if bPosition.x > prize.x || bPosition.y > prize.y {
+                        break
+                    }
+                }
+            }
+            
+            return minTotal ?? 0
+        }
+        .sum
+        .description
     }
 }
