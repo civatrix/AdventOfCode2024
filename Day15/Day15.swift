e2ddebaba92ad2f934cd0a5526667179b3fe268c
Daniel Johns <civatrix@gmail.com>
2024-12-14 21:22:33 -0800

    Day 15 Part 1

--- Day15/Day15.swift
+++ Day15/Day15.swift
@@ -9,6 +9,62 @@ import Foundation
 
 final class Day15: Day {
     func run(input: String) -> String {
-        return ""
+        let lines = input.allLines
+        var robot = Point.zero
+        var walls = Set<Point>()
+        var boxes = Set<Point>()
+        for (y, line) in lines.split(separator: "")[0].enumerated() {
+            for (x, cell) in line.enumerated() {
+                switch cell {
+                case "#":
+                    walls.insert([x, y])
+                case "O":
+                    boxes.insert([x, y])
+                case "@":
+                    robot = [x, y]
+                case ".":
+                    break
+                default:
+                    fatalError("Unknown map tile \(cell)")
+                }
+            }
+        }
+        
+        let instructions = lines.split(separator: "")[1].flatMap { $0 }.map { cell -> Point in
+            switch cell {
+            case "^": .up
+            case "v": .down
+            case "<": .left
+            case ">": .right
+            default:
+                fatalError("Unknown instruction \(cell)")
+            }
+        }
+        
+        for instruction in instructions {
+            let next = robot + instruction
+            if walls.contains(next) {
+                continue
+            } else if boxes.contains(next) {
+                var probe: Point? = next + instruction
+                while let subprobe = probe {
+                    if walls.contains(subprobe) {
+                        probe = nil
+                    } else if boxes.contains(subprobe) {
+                        probe = subprobe + instruction
+                    } else {
+                        break
+                    }
+                }
+                if let probe {
+                    robot = next
+                    boxes.remove(next)
+                    boxes.insert(probe)
+                }
+            } else {
+                robot = next
+            }
+        }
+        return boxes.map { $0.x + $0.y * 100 }.sum.description
     }
 }
--- Day15/Day15Tests.swift
+++ Day15/Day15Tests.swift
@@ -13,7 +13,45 @@ final class Day15Tests: XCTestCase {
     func testDay() throws {
         let input =
 """
+##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
+
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
 """
-        XCTAssertEqual(day.run(input: input), "")
+        XCTAssertEqual(day.run(input: input), "10092")
+    }
+    
+    func testSmall() throws {
+        let input =
+"""
+########
+#..O.O.#
+##@.O..#
+#...O..#
+#.#.O..#
+#...O..#
+#......#
+########
+
+<^^>>>vv<v>>v<<
+"""
+        XCTAssertEqual(day.run(input: input), "2028")
     }
 }
