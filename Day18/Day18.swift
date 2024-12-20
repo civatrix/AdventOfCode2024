b65cd18cf7acf93f7694aacf4c4e087ae49f9938
Daniel Johns <civatrix@gmail.com>
2024-12-20 14:28:52 -0800

    Day 18 Part 1

--- Day18/Day18.swift
+++ Day18/Day18.swift
@@ -8,7 +8,23 @@
 import Foundation
 
 final class Day18: Day {
+    var gridSize = 70
+    var dropCount = 1024
     func run(input: String) -> String {
-        return ""
+        var walls = Set<Point>()
+        for index in 0 ... gridSize {
+            walls.insert([index, -1])
+            walls.insert([-1, index])
+            walls.insert([index, gridSize + 1])
+            walls.insert([gridSize + 1, index])
+        }
+        for line in input.lines.prefix(dropCount) {
+            let numbers = line.allDigits
+            walls.insert([numbers[0], numbers[1]])
+        }
+        
+        return (AStar(graph: GridGraph(walls: walls), heuristic: Point.distance(between:and:))
+            .path(start: .zero, target: [gridSize, gridSize])
+            .count - 1).description
     }
 }
--- Day18/Day18Tests.swift
+++ Day18/Day18Tests.swift
@@ -13,7 +13,34 @@ final class Day18Tests: XCTestCase {
     func testDay() throws {
         let input =
 """
+5,4
+4,2
+4,5
+3,0
+2,1
+6,3
+2,4
+1,5
+0,6
+3,3
+2,6
+5,1
+1,2
+5,5
+2,5
+6,5
+1,4
+0,4
+6,4
+1,1
+6,1
+1,0
+0,5
+1,6
+2,0
 """
-        XCTAssertEqual(day.run(input: input), "")
+        day.gridSize = 6
+        day.dropCount = 12
+        XCTAssertEqual(day.run(input: input), "22")
     }
 }
