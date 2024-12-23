544396066029f3df7156ff3b25bc6c80b3441197
Daniel Johns <civatrix@gmail.com>
2024-12-22 23:13:14 -0800

    Day 23 Part 1

--- Day23/Day23.swift
+++ Day23/Day23.swift
@@ -9,6 +9,29 @@ import Foundation
 
 final class Day23: Day {
     func run(input: String) -> String {
-        return ""
+        var network = [String: Set<String>]()
+        
+        for line in input.lines {
+            let (lhs, rhs) = line.bifurcate(on: "-")
+            network[lhs, default: []].insert(rhs)
+            network[rhs, default: []].insert(lhs)
+        }
+        
+        for key in network.keys {
+            network[key, default: []].insert(key)
+        }
+        
+        return network.keys.combinations(ofCount: 3)
+            .filter { $0.contains(where: { $0.hasPrefix("t") }) }
+            .filter { combination in
+                for key in combination {
+                    if !network[key, default: []].isSuperset(of: combination) {
+                        return false
+                    }
+                }
+                return true
+            }
+            .count
+            .description
     }
 }
