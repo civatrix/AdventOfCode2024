1b868888c1617f8d3cd7e33571aef27650423a1c
Daniel Johns <civatrix@gmail.com>
2024-12-30 19:37:20 -0800

    Day 25 Part 1 & 2

--- Day25/Day25.swift
+++ Day25/Day25.swift
@@ -9,6 +9,37 @@ import Foundation
 
 final class Day25: Day {
     func run(input: String) -> String {
-        return ""
+        var keys = [[Int]]()
+        var locks = [[Int]]()
+        
+        for chunk in input.split(separator: "\n\n") {
+            let grid = chunk.parseGrid()
+            if grid.contains([0, 0]) {
+                var lock = [Int]()
+                for column in 0 ..< 5 {
+                    lock.append(grid.filter { $0.x == column }.map { $0.y }.max()!)
+                }
+                locks.append(lock)
+            } else if grid.contains([0, 6]) {
+                var key = [Int]()
+                for column in 0 ..< 5 {
+                    key.append(6 - grid.filter { $0.x == column }.map { $0.y }.min()!)
+                }
+                keys.append(key)
+            } else {
+                fatalError("Unknown chunk \(chunk)")
+            }
+        }
+        
+        var total = 0
+        for key in keys {
+            for lock in locks {
+                let overlaps = (0 ..< 5).contains { key[$0] + lock[$0] >= 6 }
+                if !overlaps {
+                    total += 1
+                }
+            }
+        }
+        return total.description
     }
 }
